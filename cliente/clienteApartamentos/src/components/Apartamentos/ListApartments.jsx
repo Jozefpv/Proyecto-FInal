f43f63e58c317301bcbff38b4ebb37367f4d3bb1
Jozefpv <jozefpv99@gmail.com>
2023-04-09 21:39:36 +0100

    funcionalidad de las reservas

--- cliente/clienteApartamentos/src/components/Apartamentos/ListApartments.jsx
+++ cliente/clienteApartamentos/src/components/Apartamentos/ListApartments.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import Apartment from "./Apartment"
 import { Link } from "react-router-dom"
 import './styleListApartments.css'
-import imagenes from "./imagenes"
+import imagenes from "./data/imagenes"
 
 const ListApartments = () => {
 
@@ -20,8 +20,18 @@ const ListApartments = () => {
 
     return (
         <>
-            <div style={{ backgroundColor: '#f4fafc', height: '100vh', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: '20px', padding: '15px', justifyContent:'center' }}>
-                {apartment.map((item) => <div className="listApartmentText"><Link to={`/apartamentos/${item._id}`} style={{ textDecoration: 'none', cursor: 'pointer', color: 'black', display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}><Apartment foto={imagenes[item._id]} info={item} /></Link></div>)}
+            <div style={{ backgroundColor: '#f4fafc', height: '100%', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: '20px', padding: '15px', justifyContent: 'center' }}>
+                {apartment.map((item) => {
+                    return (
+                        <div className="listApartmentText">
+                            <Link to={`/apartamentos/${item._id}`} style={{ textDecoration: 'none', cursor: 'pointer', color: 'black', display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+                                <Apartment foto={imagenes[item._id]} info={item} />
+                            </Link>
+                        </div>
+                    )
+                }
+                )
+                }
             </div >
         </>
     )
--- servidor/src/index.js
+++ servidor/src/index.js
@@ -1,7 +1,7 @@
 require('./database')
 const express = require('express')
 const morgan = require('morgan')
-
+const cors = require('cors')
 const app = express()
 
 //Settings
@@ -10,14 +10,11 @@ app.set('port', process.env.PORT || 3003)
 //middlewares
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(cors())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   });
-// async function main(){
-//     const products = await Product.find()
-//     return products
-// }
 
 app.use(require('./routes/routes'))
 
